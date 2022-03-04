13f2e413ea4b116c2d9790ddddaa9e9c6bb0f957
janvorac <vorac@mail.muni.cz>
2022-03-04 19:49:25 +0100

    more plots in the report

--- app.py
+++ app.py
@@ -28,19 +28,46 @@ def get_data() -> pd.DataFrame:
     return data.dropna(how='all', axis='index').dropna(how='all', axis='columns')
 
 
-def tummy_full_histogram(data: pd.DataFrame):
-    data = get_data()
-
+def create_tummy_full_histogram(data: pd.DataFrame):
     fig = px.histogram(data, x='jak_se_naji', category_orders=dict(jak_se_naji=[1, 2, 3, 4, 5]))
     fig.update_yaxes(title='počet')
     fig.update_xaxes(title='Jak dobře se v jídelně najíte (známky jako ve škole)')
     return fig
 
+def create_how_often_you_return_hist(data):
+    data_sub = data.copy()
+    data_sub['jak_casto_vraci'] = data_sub['jak_casto_vraci'].replace('1', 'téměř pokaždé')
+    data_sub['jak_casto_vraci'] = data_sub['jak_casto_vraci'].replace('2', '3-4×')
+    data_sub['jak_casto_vraci'] = data_sub['jak_casto_vraci'].replace('3', '1-2×')
+    data_sub['jak_casto_vraci'] = data_sub['jak_casto_vraci'].replace('4', 'výjimečně')
+    fig = px.histogram(
+        data_sub,
+        x='jak_casto_vraci',
+        category_orders=dict(
+            jak_casto_vraci=['výjimečně', '1-2×', '3-4×', 'téměř pokaždé'])
+    )
+    fig.update_yaxes(title='počet')
+    fig.update_xaxes(title='Jak často vracíte víc než polovinu jídla (z pěti obědů)')
+    return fig
+
+def create_quality_score_histogram(data):
+    fig = px.histogram(
+        data, x='kvalita', category_orders=dict(kvalita=[1, 2, 3, 4, 5])
+    )
+    fig.update_yaxes(title='počet')
+    fig.update_xaxes(title='Jak jste spokojeni s kvalitou a chutí jídla v ŠJ (známky jako ve škole)')
+    return fig
 
 def main():
     data = get_data()
-    tummy_full = tummy_full_histogram(data)
+    tummy_full = create_tummy_full_histogram(data)
     tummy_full.write_html(PLOT_PATH / 'tummy_full.html')
+
+    returns = create_how_often_you_return_hist(data)
+    returns.write_html(PLOT_PATH / 'returns.html')
+
+    quality = create_quality_score_histogram(data)
+    quality.write_html(PLOT_PATH / 'quality_score.html')
 
 
 if __name__ == "__main__":
