c0e2a1ef2b237c58b74d8b6a1a2f84d2013e977e
janvorac <vorac@mail.muni.cz>
2022-03-05 20:39:08 +0100

    more contents in the report

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import plotly.express as px
 
 
 PLOT_PATH = pathlib.Path('plots')
-
+HIST_COLOR = '#0d2a63'
 
 def get_data() -> pd.DataFrame:
     html = requests.get(
@@ -29,7 +29,12 @@ def get_data() -> pd.DataFrame:
 
 
 def create_tummy_full_histogram(data: pd.DataFrame):
-    fig = px.histogram(data, x='jak_se_naji', category_orders=dict(jak_se_naji=[1, 2, 3, 4, 5]))
+    fig = px.histogram(
+        data,
+        x='jak_se_naji',
+        category_orders=dict(jak_se_naji=[1, 2, 3, 4, 5]),
+        color_discrete_sequence=[HIST_COLOR]
+    )
     fig.update_yaxes(title='počet')
     fig.update_xaxes(title='Jak dobře se v jídelně najíte (známky jako ve škole)')
     return fig
@@ -45,7 +50,8 @@ def create_how_often_you_return_hist(data):
         data_sub,
         x='jak_casto_vraci',
         category_orders=dict(
-            jak_casto_vraci=['výjimečně', '1-2×', '3-4×', 'téměř pokaždé'])
+            jak_casto_vraci=['výjimečně', '1-2×', '3-4×', 'téměř pokaždé']),
+        color_discrete_sequence=[HIST_COLOR]
     )
     fig.update_yaxes(title='počet')
     fig.update_xaxes(title='Jak často vracíte víc než polovinu jídla (z pěti obědů)')
@@ -54,7 +60,7 @@ def create_how_often_you_return_hist(data):
 
 def create_quality_score_histogram(data):
     fig = px.histogram(
-        data, x='kvalita', category_orders=dict(kvalita=[1, 2, 3, 4, 5])
+        data, x='kvalita', category_orders=dict(kvalita=[1, 2, 3, 4, 5]),  color_discrete_sequence=[HIST_COLOR]
     )
     fig.update_yaxes(title='počet')
     fig.update_xaxes(title='Jak jste spokojeni s kvalitou a chutí jídla v ŠJ (známky jako ve škole)')
